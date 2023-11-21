1e8ee7901df917d586dbb358659696762ae90d5d
Marnien Cueba <marniencueba94@gmail.com>
2023-11-22 02:14:16 +0800

    data

---
+++ src/data/data.ts
@@ -0,0 +1,499 @@
+export const data = {
+  hero_section: {
+    img: {
+      alt: "marnien cueba profile",
+      url: "marnien-cueba.png",
+      thumbnail: "marnien-cueba.gif",
+    },
+    greeting: "Hi, I am",
+    name: "Marnien Cueba",
+    position: "UI/UX and Frontend Developer",
+    cta: "#contacts",
+  },
+  about_section: {
+    pattern: "pattern.svg",
+    title: "About Me",
+    description: [
+      `I am Marnien Cueba. You can call me Marns for short, and I live in Cebu, Philippines. An IT college graduate at the Center for Industrial Technology and Enterprise | CITE Technical Institute, Inc.`,
+      `A detail-oriented, UI/UX Designer and Frontend Developer with over 7 years of office experience, focusing on creating components that will be used across various projects, designing user interfaces, and user experience design. I am a collaborative and creative frontend developer with experience in HTML, CSS, React, JavaScript, TypeScript, and other front-end frameworks. I create dynamic and responsive user interfaces that give excellent user experiences.`,
+    ],
+  },
+  skills_section: {
+    title: "Skills",
+    skills: {
+      hard: [
+        {
+          id: "html5",
+          name: "HTML5",
+          level: "expert",
+          ratings: 5,
+        },
+        {
+          id: "css3",
+          name: "CSS3",
+          level: "expert",
+          ratings: 5,
+        },
+        {
+          id: "javascript",
+          name: "Javascript",
+          level: "intermediate",
+          ratings: 3,
+        },
+        {
+          id: "typescript",
+          name: "Typescript",
+          level: "intermediate",
+          ratings: 3,
+        },
+        {
+          id: "tailwind-css",
+          name: "Tailwind CSS",
+          level: "proficient",
+          ratings: 4,
+        },
+        {
+          id: "styled-components",
+          name: "styled-components",
+          level: "expert",
+          ratings: 5,
+        },
+        {
+          id: "sass",
+          name: "SASS",
+          level: "proficient",
+          ratings: 4,
+        },
+        {
+          id: "bootstrap",
+          name: "Bootstrap",
+          level: "intermediate",
+          ratings: 3,
+        },
+        {
+          id: "react",
+          name: "React",
+          level: "intermediate",
+          ratings: 3,
+        },
+        {
+          id: "vite-js",
+          name: "Vite JS",
+          level: "beginner",
+          ratings: 2,
+        },
+        {
+          id: "next-js",
+          name: "Next JS",
+          level: "beginner",
+          ratings: 2,
+        },
+        {
+          id: "angular",
+          name: "Angular",
+          level: "beginner",
+          ratings: 2,
+        },
+        {
+          id: "turbo-repo",
+          name: "Turborepo",
+          level: "beginner",
+          ratings: 2,
+        },
+        {
+          id: "adode-photoshop",
+          name: "Adode Photoshop",
+          level: "intermediate",
+          ratings: 3,
+        },
+        {
+          id: "adode-illustrator",
+          name: "Adode Illustrator",
+          level: "intermediate",
+          ratings: 3,
+        },
+        {
+          id: "adode-xd",
+          name: "Adode XD",
+          level: "beginner",
+          ratings: 2,
+        },
+        {
+          id: "figma",
+          name: "Figma",
+          level: "intermediate",
+          ratings: 3,
+        },
+        {
+          id: "mockflow",
+          name: "Mockflow",
+          level: "intermediate",
+          ratings: 3,
+        },
+        {
+          id: "gimp",
+          name: "Gimp",
+          level: "Proficient",
+          ratings: 4,
+        },
+        {
+          id: "inkscape",
+          name: "Inkscape",
+          level: "proficient",
+          ratings: 4,
+        },
+      ],
+      others: [
+        {
+          id: "git",
+          name: "git",
+        },
+        {
+          id: "gitlab",
+          name: "GitLab",
+        },
+        {
+          id: "github",
+          name: "GitHub",
+        },
+        {
+          id: "npm",
+          name: "NPM",
+        },
+        {
+          id: "yarn",
+          name: "yarn",
+        },
+        {
+          id: "vs-code",
+          name: "VS Code",
+        },
+        {
+          id: "sublime-text",
+          name: "sublime text",
+        },
+        {
+          id: "ring-central",
+          name: "ring central",
+        },
+        {
+          id: "azure-boards",
+          name: "azure boards",
+        },
+        {
+          id: "slack",
+          name: "slack",
+        },
+        {
+          id: "zoom",
+          name: "zoom",
+        },
+        {
+          id: "jitsi",
+          name: "jitsi",
+        },
+      ],
+      soft: [
+        {
+          id: "communication",
+          name: "communication",
+        },
+        {
+          id: "collaboration",
+          name: "collaboration",
+        },
+        {
+          id: "teamwork",
+          name: "teamwork",
+        },
+        {
+          id: "leadership",
+          name: "leadership",
+        },
+        {
+          id: "problem-solving",
+          name: "problem solving",
+        },
+        {
+          id: "attention-to-detail",
+          name: "attention to detail",
+        },
+        {
+          id: "flexibility",
+          name: "flexibility",
+        },
+        {
+          id: "creativity",
+          name: "creativity",
+        },
+        {
+          id: "critical-thinking",
+          name: "critical thinking",
+        },
+        {
+          id: "decision-making",
+          name: "decision making",
+        },
+        {
+          id: "conflict-resolution",
+          name: "conflict resolution",
+        },
+        {
+          id: "design-thinking",
+          name: "design thinking",
+        },
+        {
+          id: "proactivity",
+          name: "proactivity",
+        },
+        {
+          id: "determination",
+          name: "determination",
+        },
+        {
+          id: "organization",
+          name: "organization",
+        },
+        {
+          id: "adaptability",
+          name: "adaptability",
+        },
+        {
+          id: "analytical-thinking",
+          name: "analytical thinking",
+        },
+        {
+          id: "coaching",
+          name: "coaching",
+        },
+        {
+          id: "mentoring",
+          name: "mentoring",
+        },
+        {
+          id: "requirement-gathering",
+          name: "requirement gathering",
+        },
+      ],
+    },
+  },
+  experiences_section: {
+    title: "Experiences",
+    experiences: [
+      {
+        id: "dna-ui-ux-research-and-development-team-lead",
+        company_name: "DNA Micro Software Inc.",
+        company_address: "St. Moritz Rd, Gorordo Ave, Kamputhaw, Cebu City",
+        job_position: "UI/UX Research and Development Team Lead",
+        job_start: "September 2022",
+        job_end: "August 2023",
+        awards: {
+          label: "Received Certification of Appreciation",
+          img: "",
+        },
+        responsibilities: [
+          {
+            id: "res-1",
+            description:
+              "Developed components to be used in multiple projects.",
+          },
+          {
+            id: "res-2",
+            description:
+              "Worked with the UAT team and team members to obtain high-quality results.",
+          },
+          {
+            id: "res-3",
+            description:
+              "Worked with a direct manager to obtain fundamental requirements for each component based on project requirements.",
+          },
+        ],
+      },
+      {
+        id: "dna-software-engineer-ui-team-lead-r&d",
+        company_name: "DNA Micro Software Inc.",
+        company_address: "St. Moritz Rd, Gorordo Ave, Kamputhaw, Cebu City",
+        job_position: "Software Engineer UI Team Lead (R&D)",
+        job_start: "July 2022",
+        job_end: "September 2022",
+        awards: {
+          label: "Received Certification of Appreciation",
+          img: "",
+        },
+        responsibilities: [
+          {
+            id: "res-1",
+            description:
+              "Conducted research on fundamental requirements for each component.",
+          },
+          {
+            id: "res-2",
+            description:
+              "Worked closely with the supervisor to obtain high-quality results.",
+          },
+          {
+            id: "res-3",
+            description:
+              "Developed spikes for complex components and once approved, it will be added to Storybook and will be used in actual sprint development.",
+          },
+        ],
+      },
+      {
+        id: "dna-software-engineer-ui-team-lead",
+        company_name: "DNA Micro Software Inc.",
+        company_address: "St. Moritz Rd, Gorordo Ave, Kamputhaw, Cebu City",
+        job_position: "Software Engineer UI Team Lead",
+        job_start: "June 2021",
+        job_end: "July 2022",
+        awards: {
+          label: "",
+          img: "",
+        },
+        responsibilities: [
+          {
+            id: "res-1",
+            description:
+              "Led daily team meetings to discuss the objectives and top priorities.",
+          },
+          {
+            id: "res-2",
+            description:
+              "Worked with team members to provide high-quality results.",
+          },
+          {
+            id: "res-3",
+            description:
+              "Worked with a direct manager to obtain fundamental requirements for each component based on project requirements.",
+          },
+        ],
+      },
+      {
+        id: "dna-software-engineer-ui",
+        company_name: "DNA Micro Software Inc.",
+        company_address: "St. Moritz Rd, Gorordo Ave, Kamputhaw, Cebu City",
+        job_position: "Software Engineer UI",
+        job_start: "March 2020",
+        job_end: "June 2021",
+        awards: {
+          label: "",
+          img: "",
+        },
+        responsibilities: [
+          {
+            id: "res-1",
+            description:
+              "Established design guidelines, standards, and best practices.",
+          },
+          {
+            id: "res-2",
+            description:
+              "Maintained the appearance of websites by enforcing content standards.",
+          },
+          {
+            id: "res-3",
+            description:
+              "Communicated design ideas using user and process flows, sitemaps, and wireframes.",
+          },
+        ],
+      },
+      {
+        id: "ogt-frontend-developer",
+        company_name: "Our Global Team",
+        company_address: "Cebu Business Park - Globe Tower Building",
+        job_position: "Frontend Developer",
+        job_start: "March 2019",
+        job_end: "March 2020",
+        awards: {
+          label: "",
+          img: "",
+        },
+        responsibilities: [
+          {
+            id: "res-1",
+            description:
+              "Established design guidelines, standards and best practices.",
+          },
+          {
+            id: "res-2",
+            description: "Tested and improved the design of the website.",
+          },
+          {
+            id: "res-3",
+            description:
+              "Maintained the appearance of websites by enforcing content standards.",
+          },
+          {
+            id: "res-4",
+            description:
+              "Took responsibility for responsiveness on both web and mobile pages and designs.",
+          },
+        ],
+      },
+      {
+        id: "lc-ui-ux-web-and-graphic-designer",
+        company_name: "Litecloud Corporation",
+        company_address: "Sanson Rd, Lahug, Cebu City",
+        job_position: "UI/UX / Web and Graphic Designer",
+        job_start: "January 2018",
+        job_end: "March 2019",
+        awards: {
+          label: "",
+          img: "",
+        },
+        responsibilities: [
+          {
+            id: "res-1",
+            description: "Trained multiple batches of newly hired interns.",
+          },
+          {
+            id: "res-2",
+            description:
+              "Designed debit cards for a sister company (Equicredit).",
+          },
+          {
+            id: "res-3",
+            description: "Designed emergency signages for building purposes.",
+          },
+        ],
+      },
+      {
+        id: "lc-ui-ux-web-and-graphic-designer-intern",
+        company_name: "Litecloud Corporation",
+        company_address: "Sanson Rd, Lahug, Cebu City",
+        job_position: "UI/UX / Web and Graphic Designer (Intern)",
+        job_start: "July 2016",
+        job_end: "January 2018",
+        awards: {
+          label: "",
+          img: "",
+        },
+        responsibilities: [
+          {
+            id: "res-1",
+            description: "Redesigned Litecloud website.",
+          },
+          {
+            id: "res-2",
+            description:
+              "Redesigned company ID including sister company - Equicredit.",
+          },
+          {
+            id: "res-3",
+            description:
+              "Designed icons, logos, and mockups to be used across multiple projects.",
+          },
+          {
+            id: "res-4",
+            description:
+              "Designed banners, brochures, and magazines for marketing purposes.",
+          },
+          {
+            id: "res-5",
+            description:
+              "Designed visual imagery for websites and ensured that they were in line with clients` branding.",
+          },
+        ],
+      },
+    ],
+  },
+};
