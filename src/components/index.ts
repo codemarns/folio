a173b328d536a68d347794d715f55281908d1ab5
Marnien Cueba <marniencueba94@gmail.com>
2023-11-22 02:14:05 +0800

    componnet index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 export { Button } from "./button/button";
 export { Icon } from "./icon/icon";
+export { Image } from "./image/image";
+export { Title } from "./title/title";
+
 export { ScrollToTop } from "./scroll/scroll";
