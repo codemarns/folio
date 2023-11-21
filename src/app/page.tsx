f5c7169f47af905feadba21746289c198fdbfc97
Marnien Cueba <marniencueba94@gmail.com>
2023-11-22 02:10:58 +0800

    Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { About, Hero } from "./home";
+import { data } from "@/data/data";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World!</h1>
-    </main>
+    <>
+      <Hero data={data.hero_section} />
+      <About data={data.about_section} />
+    </>
   );
 }
