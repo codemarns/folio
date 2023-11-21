927ea7a15975d8ff028b5b1cf4e1c04cdcbc8e2e
Marnien Cueba <marniencueba94@gmail.com>
2023-11-22 02:15:17 +0800

    styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,2 +1,10 @@
 export { buttonStyles } from "./buttonStyles";
 export { iconStyles } from "./iconStyles";
+export { imageStyles } from "./imageStyles";
+export { titleStyles } from "./titleStyles";
+
+export { heroStyles } from "./sections/heroStyles";
+export { aboutStyles } from "./sections/aboutStyles";
+
+export { sectionStyles } from "./layouts/sectionStyles";
+export { containerStyles } from "./layouts/containerStyles";
