fdecd2541fbba8d576836fcfe6d4ce0036ff758e
Marnien Cueba <marniencueba94@gmail.com>
2023-11-23 12:35:50 +0800

    Designs: initial design

--- src/app/designs/page.tsx
+++ src/app/designs/page.tsx
@@ -4,6 +4,7 @@ import { Hero, Works } from "./sections";
 export default function Designs() {
   return (
     <>
+      <div className="h-16 w-full" />
       <Hero />
       <Works />
     </>
--- src/app/designs/sections/works/works.tsx
+++ src/app/designs/sections/works/works.tsx
@@ -1,35 +1,147 @@
+"use client";
+
+import cn from "classnames";
+import React, { useMemo, useState } from "react";
+import { Button, Image, Title } from "@/components";
 import { Container, Section } from "@/common";
-import { Image, Title } from "@/components";
-import React from "react";
+
+type Tag = "all" | "layout" | "component" | "css";
+
+const arr = [
+  {
+    id: "button",
+    topic: "Button",
+    tag: "component",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+    blanditiis odit iste maxime odio! Reprehenderit saepe ipsum,
+    impedit, sapiente eaque neque voluptate eos, aut doloribus
+    architecto sunt voluptates nesciunt placeat.`,
+  },
+  {
+    id: "z-pattern-layout",
+    topic: "Z Pattern Layout",
+    tag: "layout",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+    blanditiis odit iste maxime odio! Reprehenderit saepe ipsum,
+    impedit, sapiente eaque neque voluptate eos, aut doloribus
+    architecto sunt voluptates nesciunt placeat.`,
+  },
+  {
+    id: "zoom-animation",
+    topic: "Zoom Animation",
+    tag: "css",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+    blanditiis odit iste maxime odio! Reprehenderit saepe ipsum,
+    impedit, sapiente eaque neque voluptate eos, aut doloribus
+    architecto sunt voluptates nesciunt placeat.`,
+  },
+  {
+    id: "image",
+    topic: "Image",
+    tag: "component",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+    blanditiis odit iste maxime odio! Reprehenderit saepe ipsum,
+    impedit, sapiente eaque neque voluptate eos, aut doloribus
+    architecto sunt voluptates nesciunt placeat.`,
+  },
+  {
+    id: "single-column-layout",
+    topic: "Single Column Layout",
+    tag: "layout",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+    blanditiis odit iste maxime odio! Reprehenderit saepe ipsum,
+    impedit, sapiente eaque neque voluptate eos, aut doloribus
+    architecto sunt voluptates nesciunt placeat.`,
+  },
+  {
+    id: "card",
+    topic: "Card",
+    tag: "component",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+    blanditiis odit iste maxime odio! Reprehenderit saepe ipsum,
+    impedit, sapiente eaque neque voluptate eos, aut doloribus
+    architecto sunt voluptates nesciunt placeat.`,
+  },
+];
+
+const tabMenu = [
+  {
+    id: "all",
+    label: "All",
+  },
+  {
+    id: "layout",
+    label: "Layout",
+  },
+  {
+    id: "component",
+    label: "Component",
+  },
+  {
+    id: "css",
+    label: "CSS",
+  },
+];
 
 export const Works = () => {
-  const data = [];
-  const data_length = 25;
-  for (let i = 0; i < data_length; i++) {
-    data.push(
-      <div
-        key={i}
-        className="h-auto p-4 bg-secondary-900 rounded-xl overflow-hidden hover:scale-105 duration-300 ease-in-out transition-all"
-      >
-        <Image
-          alt="dummy"
-          src="https://dummyimage.com/600x400/1e0a2c/4b5569.jpg"
-          thumbnail=""
-          aspectRatio="4:3"
-          corner="soft-edge"
-        />
-        <div className="h-[150px] w-full p-4"></div>
-      </div>
-    );
-  }
+  const [filterData, setFilterData] = useState<Tag>("all");
+
+  const filteredData = useMemo(() => {
+    if (filterData === "component")
+      return arr?.filter((e) => e.tag === "component");
+    if (filterData === "layout") return arr?.filter((e) => e.tag === "layout");
+    if (filterData === "css") return arr?.filter((e) => e.tag === "css");
+    return arr;
+  }, [filterData]);
 
   return (
-    <Section id="hero">
-      <Container id="hero-container">
+    <Section id="works" className=" ">
+      <Container id="works-container">
         <Title name="Designs" />
 
+        <div className="flex items-center justify-center gap-2 border-b border-secondary-900/50">
+          {tabMenu.map((menu) => (
+            <Button
+              key={menu.id}
+              label={menu.label}
+              onClick={() => setFilterData(menu.id as Tag)}
+              className={cn(
+                "!bg-transparent !text-current border-2 border-b-4 border-x-transparent border-t-transparent rounded-none",
+                filterData === menu.id
+                  ? "border-secondary"
+                  : "border-transparent"
+              )}
+            />
+          ))}
+        </div>
+
         <div className="grid grid-cols-1 sm:grid-cols-[repeat(auto-fill,_minmax(285px,_1fr))] gap-12">
-          {data}
+          {filteredData.map((item) => (
+            <div
+              key={item.id}
+              className="group h-auto p-4 space-y-5 bg-secondary-900 rounded-xl shadow overflow-hidden hover:shadow-md duration-300 ease-in-out transition-all"
+            >
+              <Image
+                alt="dummy"
+                src="best-websites-for-color-theme-picker-video.png"
+                thumbnail=""
+                aspectRatio="4:3"
+                corner="soft-edge"
+              />
+              <div className="min-h-[150px] w-full space-y-4 text-left">
+                <div className="space-y-2">
+                  <span className="block leading-none text-lg font-normal text-secondary">
+                    {item.topic}
+                  </span>
+                  <span className="block leading-none text-sm uppercase opacity-50">
+                    {item.tag}
+                  </span>
+                </div>
+                <p className="line-clamp-4">{item.description}</p>
+                <Button label="See Details" icon="arrow" layout="block" />
+              </div>
+            </div>
+          ))}
         </div>
       </Container>
     </Section>
