8806581339a482f6a2e7d41d159c4caada1c8c73
Marnien Cueba <marniencueba94@gmail.com>
2023-11-24 17:17:39 +0800

    design page: enhancements

--- src/styles/pages/designs/designsStyles.ts
+++ src/styles/pages/designs/designsStyles.ts
@@ -5,7 +5,7 @@ export const designsStyles = {
   tabMenu: {
     base: "flex items-center justify-center gap-2 sm:gap-4 border-b border-default-900",
     item: {
-      base: "!bg-transparent !text-current text-sm xs:text-base !px-1 border-2 border-b-4 !border-x-transparent !border-t-transparent rounded-none whitespace-nowrap",
+      base: "!bg-transparent !text-current text-sm xs:text-base sm:!h-14 !px-1 sm:!px-4 border-2 border-b-4 !border-x-transparent !border-t-transparent rounded-none whitespace-nowrap",
       active: "border-secondary !text-secondary",
       inactive:
         "border-transparent !text-default hover:border-secondary hover:!text-default",
--- src/styles/pages/designs/heroStyles.ts
+++ src/styles/pages/designs/heroStyles.ts
@@ -1,7 +1,7 @@
 export const heroStyles = {
   root: {
     // #302d33
-    base: "relative min-h-[500px] flex items-center justify-center bg-[#26212b]",
+    base: "relative min-h-[500px] flex items-center justify-center bg-gradient-to-br from-[#26212b]/75 via-[#26212b] to-[#26212b]/75", // bg-[#26212b]
   },
   img: {
     base: "absolute -bottom-0 right-4 sm:right-8 md:right-16 w-[100px] sm:w-[150px] md:w-[200px] opacity-10 duration-150 ease-in-out transition-all",
