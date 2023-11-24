1f6c7aa4d59f084d6200b0bbd44d468c62d231a6
Marnien Cueba <marniencueba94@gmail.com>
2023-11-24 13:39:37 +0800

    transfer home styling

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,14 +1,19 @@
+// COMPONENT
 export { buttonStyles } from "./buttonStyles";
 export { iconStyles } from "./iconStyles";
 export { imageStyles } from "./imageStyles";
 export { titleStyles } from "./titleStyles";
 
-export { heroStyles } from "./sections/heroStyles";
-export { aboutStyles } from "./sections/aboutStyles";
-export { skillsStyles } from "./sections/skillsStyles";
-export { experiencesStyles } from "./sections/experiencesStyles";
-export { contactsStyles } from "./sections/contactsStyles";
-
+// COMMON
 export { layoutStyles } from "./layouts/layoutStyles";
 export { sectionStyles } from "./layouts/sectionStyles";
 export { containerStyles } from "./layouts/containerStyles";
+
+// HOME
+export { heroStyles } from "./pages/home/heroStyles";
+export { aboutStyles } from "./pages/home/aboutStyles";
+export { skillsStyles } from "./pages/home/skillsStyles";
+export { experiencesStyles } from "./pages/home/experiencesStyles";
+export { contactsStyles } from "./pages/home/contactsStyles";
+
+// DESIGNS
