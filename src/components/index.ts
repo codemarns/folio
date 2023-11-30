61113c045c4885e4208a03eff5dffa14da160faa
Marnien Cueba <marniencueba94@gmail.com>
2023-11-30 14:40:17 +0800

    export Image Props

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@ export { Button } from "./button/button";
 export { Card } from "./card/card";
 export { CardV2 } from "./card/cardV2";
 export { Icon } from "./icon/icon";
-export { Image } from "./image/image";
+export { Image, type ImageProps } from "./image/image";
 export { Title } from "./title/title";
 
 export { ScrollToTop } from "./scroll/scroll";
