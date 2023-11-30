479c207da3e75b150b2384db8423c09548cffaf4
Marnien Cueba <marniencueba94@gmail.com>
2023-11-30 14:39:49 +0800

    custom card

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { Button } from "./button/button";
 export { Card } from "./card/card";
+export { CardV2 } from "./card/cardV2";
 export { Icon } from "./icon/icon";
 export { Image } from "./image/image";
 export { Title } from "./title/title";
