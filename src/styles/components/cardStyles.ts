2752d54790d75e05879361320a4baeadcbc5d37c
Marnien Cueba <marniencueba94@gmail.com>
2023-11-30 14:39:25 +0800

    Card: enhance UI

--- src/styles/components/cardStyles.ts
+++ src/styles/components/cardStyles.ts
@@ -2,7 +2,7 @@ export const cardStyles = {
   root: {
     base: "group relative h-auto bg-[#26212b] border border-[#26212b] rounded-2xl shadow overflow-hidden duration-300 ease-in-out transition-all",
     img: {
-      base: "saturate-50 brightness-50 group-hover:saturate-100 group-hover:brightness-100 duration-300 ease-in-out transition-all",
+      base: "saturate-[.75] brightness-[.75] group-hover:saturate-100 group-hover:brightness-100 duration-300 ease-in-out transition-all",
     },
     details: {
       base: "relative min-h-[150px] w-full p-5 space-y-4 text-left text-default-50",
@@ -12,7 +12,7 @@ export const cardStyles = {
       primaryInfo: {
         base: "!mt-0 space-y-2",
         title: {
-          base: "block leading-none text-xl font-semibold group-hover:text-secondary duration-300 ease-in-out transition-all",
+          base: "line-clamp-1 block leading-none text-xl font-semibold group-hover:text-secondary duration-300 ease-in-out transition-all",
         },
         tag: {
           base: "block leading-none text-sm uppercase opacity-50",
